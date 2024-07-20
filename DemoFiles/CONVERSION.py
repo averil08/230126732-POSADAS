a3c62e49a02a2eee2aa49684eca21dc595598a38
Averil Posadas <averilroshan08@gmail.com>
2024-07-20 20:26:13 +0800

    Added DemoFiles and Sample Files

---
+++ DemoFiles/CONVERSION.py
@@ -0,0 +1,8 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+# Sample usage
+celsius_temp = 25
+fahrenheit_temp = celsius_to_fahrenheit(celsius_temp)
+print(f"{celsius_temp}°C is equal to {fahrenheit_temp}°F")
+
