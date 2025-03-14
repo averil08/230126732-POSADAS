7f99c6f23ac6b5aba70358773946784b0ca54057
Alwin Rich R Villanueva <AlwinRich90@gmail.com>
2025-03-14 09:50:55 +0800

    Modified php file

--- PHP/exercise.php
+++ PHP/exercise.php
@@ -26,15 +26,45 @@
             echo "<br>$num is odd <br>";
         }
 
-        //EXERCISE 3
-        echo "<br> EXERCISE 3";
-        for ($x = 1; $x <=100; $x++) {
-            echo "<br> $x <br>";
-        }
+        // EXERCISE 3
+        echo "<br> EXERCISE 3<br>";
+        $i = 1; 
+        while ($i <= 100) {  
+            $word = ''; // Initialize the word string
+
+            if ($i % 3 == 0) {
+                $word .= 'Fizz'; 
+            }
+            
+            if ($i % 5 == 0) {
+                $word .= 'Buzz'; 
+            }
+
+            if ($word == '') {
+                echo "<br>". $i; //if not %3,5 print number
+            } else {
+                echo "<br>" . $word; // Fizz, Buzz, or FizzBuzz
+            }
+
+            $i++; // Increment the counter
+    }
+
 
         //EXERCISE 4
         echo "<br> EXERCISE 4";
-        
+        function fib ($n) { // a function called fib, declaire variable n (the sequence number)
+            for ($n=0;$n<30;$n++) {
+                if ($n < 3) { return $n; } // if n is smaller than 3 return n (1 or 2)
+                else { return fib ($n - 1) + fib ($n - 2); } 
+            /* if the number is 3 or above do 2 sums (n-1) and (n-2)
+            and then add the 2 sums together (n-1)+(n-2)
+            Example Fibonacci number 4
+            (4-1)+(4-2) = 5
+            3 + 2 = 5
+            */
+        }
+        print $n;
+    }
     ?>
 </body>
 </html>
