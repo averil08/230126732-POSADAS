319e35d54570dbf903abae13ee5a90c0ed7bba72
M306User <M306User@lab.uc-bcf.local>
2025-09-27 08:19:07 +0800

    push index.html BIODATA

---
+++ PHP/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Demo</title>
+</head>
+<body>
+    <h1>Biodata</h1>
+    <?php
+        echo "Personal Information";
+        echo "<ul>";
+        echo "<li>Name: Averil Wiw</li>";
+        echo "<li>Age: 20</li>";
+        echo "<li>Address: #190 Wiw Wawie, Engineer's Hill</li>";
+        echo "<li>Phone: 0987680982312</li>";
+        echo "<li>Email: averilwiw08@gmail.com</li>";
+        echo "</ul>";  
+    ?>
+
+    <h2>Educational Background:</h2>
+    <?php
+     echo "<ul>";
+     echo "<li>Undergrad: University of the Cordilleras (ongoing)</li>";
+     echo "<li>Senior High: Wiw BeDS</li>";
+     echo "<li>High School: Saint Wiw Academy</li>";
+     echo "</ul>";
+    ?>
+</body>
+</html>
