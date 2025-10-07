9ebab8e3f365380b50034c01b7c8359458f4cb5c
M306User <M306User@lab.uc-bcf.local>
2025-10-07 08:29:54 +0800

    push finished php exercise (#1-12)

--- PHP/php_exercise/index.php
+++ PHP/php_exercise/index.php
@@ -38,10 +38,97 @@
 
     echo "<h3>Swapping Variables</h3>
     ";
-    $celsius = 20;
-    $fahrenheit = $length * 9/5 + 32;
-    $perimeter =  2 * ($length + $width); 
-    echo "Fahrenheit is " . $fahrenheit . " °F"; 
+    $varA = 10;
+    $varB = 20;
+    echo "Before swap: <br> varA = " . $varA . ", varB = " . $varB; 
+    $temp = $varA; //temporary variab;e
+    $varA = $varB;
+    $varB = $temp;
+    
+    echo "<br>After swapping: ";
+    echo "<br>Variable A: " . $varA . "\n";
+    echo "<br>Variable B: " . $temp . "\n";
+
+    echo "<h3>Salary Calculator</h3>
+    ";
+    $basic_salary = 30000;
+    $allowance = 1000;
+    $deduction=  2000; 
+    $total = $basic_salary - ($deduction + $allowance);
+    echo "Net salary = " . $total; 
+
+    echo "<h3>BMI Calculator</h3>
+    ";
+    $weight = 70;
+    $height = 1.75;
+    $BMI =  $weight / ($height); 
+    echo "weight = " . $weight . "kg <br>";
+    echo "height = " . $height . "m <br>"; 
+    echo "BMI = " . $BMI; 
+    
+    echo "<h3>String Manipulation</h3>
+    ";
+    $sentence = "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.";
+    echo "number of character: " . (strlen($sentence));
+    echo "<br>number of character: " . (str_word_count($sentence));
+
+    echo "<h3>Bank Account Simulation</h3>
+    ";
+    $balance = 1000;
+    $deposit= 100;
+    $balance_dep = $balance + $deposit;
+    $current = $balance_dep;
+    $withdraw = 10;
+    $balance_with = $current - $withdraw;
+    echo "Deposit: " . $deposit;
+    echo "<br>Updated balance: " . $balance_dep;
+    echo "<br>Withdrawal: " . $withdraw;
+    echo "<br>Updated balance: " . $balance_with;
+
+    echo "<h3>Simple Grading System</h3>
+    ";
+    $math = 80;
+    $english = 70;
+    $science =  90;
+    $avg = ($math + $english + $science)/3; 
+    
+    if ($avg >= 90) {
+        $grade = 'A';
+    } elseif ($avg >= 80) {
+        $grade = 'B';
+    } elseif ($avg >= 70) {
+        $grade = 'C';
+    } elseif ($avg >= 60) {
+        $grade = 'D';
+    } else {
+        $grade = 'F';
+    }
+
+    echo "Math score: " . $math . "<br>";
+    echo "English score: " . $english . "<br>";
+    echo "science score: " . $science . "<br>";
+    echo "average score: " . number_format($avg, 2);
+
+    
+    echo "<h3>Currency Converter</h3>
+    ";
+    $PHP = 1000;
+    $USD = 0.01717333;
+    $EUR = 0.01466;
+    $php_usd = $PHP * $USD;
+    $php_eur = $PHP * $EUR;
+
+    echo "PHP (1000) to USD = " . $php_usd . "US dollars <br>";
+    echo "PHP (1000) to EUR = " . $php_eur . " EUR <br>";
+
+    echo "<h3>Travel Cost Estimator</h3>
+    ";
+    $distance = 200;
+    $fuel_consumption = 8;
+    $fuel_price = 4;
+    $cost = $fuel_consumption *$fuel_price;
+
+    echo "Estimated Travel Cost distance (200), fuel consumption (8), 'fuel price (4) <br>= " . $cost . " dollars";
     ?> 
 </body>
 </html>
